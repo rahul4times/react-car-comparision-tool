0eed24b9013423d63f7c0efad768efe155e4937e
rahul4times <tkdmaster.sharma@gmail.com>
2017-11-29 08:42:36 -0700

    readme file updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Hello</div>
+      </div>
+    );
+  }
+}
+
+export default App;
