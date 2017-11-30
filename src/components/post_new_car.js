fcd617a8d3e61fdcb15ba89579ff14c8682ec79d
rahul4times <tkdmaster.sharma@gmail.com>
2017-11-29 23:54:27 -0700

    validation added to form

--- src/components/post_new_car.js
+++ src/components/post_new_car.js
@@ -11,12 +11,19 @@ class PostNewCar extends Component{
           type="text"
           {...field.input}
         />
+
+        {/*--+--* Errors are coming from errors object *--+--*/}
+        {field.meta.errors}
       </div>
     );
   }
+  onSubmit(values){
+    console.log('values: ', values);
+  }
   render(){
+    const { handleSubmit } = this.props;
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <br/>
         <Field
           label="Year "
@@ -53,10 +60,45 @@ class PostNewCar extends Component{
           name="photo_url"
           component={this.renderField}
         />
+        <br/>
+        <button type="submit">Add</button>
       </form>
     );
   }
 }
+
+function validate(values){
+  // creating error object to show errors
+  const errors = {};
+
+  // validating inputs from values
+  if(!values.year){
+    errors.year = "Enter year!";
+  }
+  if(!values.make){
+    errors.make = "Enter make!";
+  }
+  if(!values.model){
+    errors.model = "Enter model!";
+  }
+  if(!values.miles){
+    errors.miles = "Enter miles!";
+  }
+  if(!values.price){
+    errors.price = "Enter price!";
+  }
+  if(!values.photo_url){
+    errors.photo_url = "Enter image url!";
+  }
+
+  // if error object is empty then redux-form is ready to submit
+  // if there is any value in error object then redux-form will stop immidiatly
+  return errors;
+}
+
 export default reduxForm({
+  // validate function is coming from above
+  // because key validate and value validate was same so just validate is below
+  validate,
   form: 'AddNewCar'
 })(PostNewCar);
